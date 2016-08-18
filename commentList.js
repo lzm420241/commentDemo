489f8225218676629500e59d21aa66513d46e923
lzm420241 <lzm420241@gmail.com>
2016-08-18 23:04:42 +0800

    修改组件共享error，增加state功能

--- commentList.js
+++ commentList.js
@@ -1,6 +1,29 @@
-import { Comment } from './comment'
+class Comment extends React.Component {
+	render() {
+		return (
+			<div className="comment">
+				<p className="comment-header">{this.props.author}</p>
+				<p className="comment-body">
+					{this.props.body}
+				</p>
+				<div className="comment-footer">
+					<a href="#" className="comment-footer-delete">
+						Delete comment
+					</a>
+				</div>
+			</div>
+		)
+	}
+}
 
 class CommentBox extends React.Component {
+
+	constructor() {
+		super();
+		this.state = {
+			showComments: false
+		};
+	}
 
 	_getComments() {
 		const commentList = [
@@ -24,15 +47,31 @@ class CommentBox extends React.Component {
 		}
 	}
 
+	_handClick() {
+		this.setState({
+			showComments: !this.state.showComments
+		});
+	}
+
 	render() {
 		const comments = this._getComments();
+		let commentNodes;
+		if( this.state.showComments ){
+			commentNodes = <div className="comment-list">{ comments }</div>		
+		}
+		let buttonText = 'Show comments';
+		if(this.state.showComments) {
+			buttonText = 'Hide comments';
+		}
 		return (
 			<div className="comment-box">
 				<h3>Comments</h3>
 				<h4 className="comment-count">{ this._getCommentsTitle(comments.length) }</h4>
+				{commentNodes}
 				<div className="comment-list">
 					{comments}
 				</div>
+				<button onClick={this._handClick.bind(this)}>{buttonText}</button>
 			</div>
 		)
 	}
